94037295892060ef7191a51246990872ada9235d
russelrex <russelrex_23@yahoo.com>
2021-06-09 00:13:44 +0800

    Update the site redirect url on email to caprover staging url.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  site: 'https://bizsolutions.app',
+  site: 'http://bizsolutions-dashboard-staging.paas.seethisnow.tech',
   production: true,
   apiUrl: 'http://bizsolutions-api-staging.paas.seethisnow.tech'
 };
