3ae1ddcaaee7d0b815d3bfad01729d687073bef7
russelrex <russelrex_23@yahoo.com>
2021-06-08 23:22:36 +0800

    Fixed the typo error on api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   site: 'https://bizsolutions.app',
   production: true,
-  apiUrl: 'http://bizsolutions-api-staging.paas.seethisnow.tech/'
+  apiUrl: 'http://bizsolutions-api-staging.paas.seethisnow.tech'
 };
